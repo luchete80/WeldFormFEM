a94069dcc36f0be3402b7b3b90339898ba3057c1
luchete80 <luciano.buglioni@gmail.com>
2023-05-09 14:49:59 -0300

    dummy

--- src/ElementCalcs.f90
+++ src/ElementCalcs.f90
@@ -1,8 +1,12 @@
-subroutine calculate_strains ()
+subroutine calculate_strain ()
   integer :: e
   do while (e < elem_count)
     !!Assemble element displacement
     !elem%strain(e,:) = matmul(elem%bl(e,:),elem)
     e = e + 1 
   end do   
+end subroutine
+
+subroutine calculate_strain_rate ()
+
 end subroutine
