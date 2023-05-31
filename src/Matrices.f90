b2820a6a70d98dca3c39ad3959586193e6474ec5
luchete80 <luciano.buglioni@gmail.com>
2023-05-31 08:40:29 -0300

    added 3x3 matrix inversion for 3d element

--- src/Matrices.f90
+++ src/Matrices.f90
@@ -16,6 +16,58 @@ contains
   ! end do
 ! end subroutine
 
+!***********************************************************************************************************************************
+!  M33INV  -  Compute the inverse of a 3x3 matrix.
+!
+!  A       = input 3x3 matrix to be inverted
+!  AINV    = output 3x3 inverse of matrix A
+!  OK_FLAG = (output) .TRUE. if the input matrix could be inverted, and .FALSE. if the input matrix is singular.
+!***********************************************************************************************************************************
+
+subroutine M33INV (A, AINV, OK_FLAG)
+
+  IMPLICIT NONE
+
+  DOUBLE PRECISION, DIMENSION(3,3), INTENT(IN)  :: A
+  DOUBLE PRECISION, DIMENSION(3,3), INTENT(OUT) :: AINV
+  LOGICAL, INTENT(OUT) :: OK_FLAG
+
+  DOUBLE PRECISION, PARAMETER :: EPS = 1.0D-10
+  DOUBLE PRECISION :: DET
+  DOUBLE PRECISION, DIMENSION(3,3) :: COFACTOR
+
+
+  DET =   A(1,1)*A(2,2)*A(3,3)  &
+        - A(1,1)*A(2,3)*A(3,2)  &
+        - A(1,2)*A(2,1)*A(3,3)  &
+        + A(1,2)*A(2,3)*A(3,1)  &
+        + A(1,3)*A(2,1)*A(3,2)  &
+        - A(1,3)*A(2,2)*A(3,1)
+
+  IF (ABS(DET) .LE. EPS) THEN
+     AINV = 0.0D0
+     OK_FLAG = .FALSE.
+     RETURN
+  END IF
+
+  COFACTOR(1,1) = +(A(2,2)*A(3,3)-A(2,3)*A(3,2))
+  COFACTOR(1,2) = -(A(2,1)*A(3,3)-A(2,3)*A(3,1))
+  COFACTOR(1,3) = +(A(2,1)*A(3,2)-A(2,2)*A(3,1))
+  COFACTOR(2,1) = -(A(1,2)*A(3,3)-A(1,3)*A(3,2))
+  COFACTOR(2,2) = +(A(1,1)*A(3,3)-A(1,3)*A(3,1))
+  COFACTOR(2,3) = -(A(1,1)*A(3,2)-A(1,2)*A(3,1))
+  COFACTOR(3,1) = +(A(1,2)*A(2,3)-A(1,3)*A(2,2))
+  COFACTOR(3,2) = -(A(1,1)*A(2,3)-A(1,3)*A(2,1))
+  COFACTOR(3,3) = +(A(1,1)*A(2,2)-A(1,2)*A(2,1))
+
+  AINV = TRANSPOSE(COFACTOR) / DET
+
+  OK_FLAG = .TRUE.
+
+RETURN
+
+end subroutine M33INV
+
 function det (a)
   real(fp_kind), dimension(dim,dim), intent (in) :: a 
   real(fp_kind) :: det
