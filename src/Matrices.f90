dcecb36ada5496ccab56b9aad7fb0f5b3070d324
luciano nb <luciano.buglioni@gmail.com>
2023-06-13 19:26:49 -0300

    fixed matrix derivatives

--- src/Matrices.f90
+++ src/Matrices.f90
@@ -338,7 +338,8 @@ subroutine calculate_element_derivMat ()
           elem%dHxy_detJ(e,gp,:,6) =  invJ(:,1)-invJ(:,2)+invJ(:,3)
           elem%dHxy_detJ(e,gp,:,7) =  invJ(:,1)+invJ(:,2)+invJ(:,3)
           elem%dHxy_detJ(e,gp,:,8) = -invJ(:,1)+invJ(:,2)+invJ(:,3)
-          
+
+          elem%dHxy_detJ(e,gp,:,:) = elem%dHxy_detJ(e,gp,:,:) * 0.125d0          
           ! print *, "1,1", elem%dHxy(e,gp,1,1), "inv 1,1 1,2 1,3", invJ(1,1), invJ(1,2), invJ(1,3)
           ! print *, "1,1", elem%dHxy(e,gp,2,1), "inv 1,1 1,2 1,3", invJ(2,1), invJ(2,2), invJ(2,3)
           do k=1,nodxelem
