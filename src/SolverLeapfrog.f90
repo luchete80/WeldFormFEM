1b8616af34fc584d5d45d39774709070eeff7c86
luchete80 <luciano.buglioni@gmail.com>
2023-06-29 15:14:43 -0300

    cleaned leapfrog

--- src/SolverLeapfrog.f90
+++ src/SolverLeapfrog.f90
@@ -122,10 +122,10 @@ subroutine SolveLeapfrog (tf, dt)
   !!!!!!!!!!!!!! CALCULATE Ku0 = RINT0, Initial internal forces
   print *, "Assemblying forces..."
   call assemble_forces()
-  ! do n=1,node_count
-      ! nod%a(n,:) = (fext_glob(n,:)-rint_glob(n,:))/mdiag(n) 
-      ! print *, "fext n ", n, fext_glob(n,:)
-  ! end do
+  do n=1,node_count
+      nod%a(n,:) = (fext_glob(n,:)-rint_glob(n,:))/mdiag(n) 
+      print *, "fext n ", n, fext_glob(n,:)
+  end do
   ! do n=1,node_count
     ! print *, "Initial accel ", n, "a ", nod%a(n,:)  
   ! end do  
@@ -149,68 +149,11 @@ subroutine SolveLeapfrog (tf, dt)
     step = step + 1
     print *, "Time: ", time, ", step: ",step, "---------------------------------------------------------"
 
-    !print *, "det EXT(e,gp)", elem%detJ(:,:)
-    do n=1,elem_count
-      if (elem%gausspc(n) .eq. 8) then !!!! ELSE IS CONSTANT
-        call calculate_element_shapeMat() !AND MASS
-      end if
-    end do
-    
-    
-    !!! TEST 
-    !xtest(:,:) = []
-    
-    ! call calculate_element_matrices()!ATTENTION: THIS CALCULATES KNL AND KL AND THIS REQUIRES UPDATE CAUCHY STRESS TAU
-    ! !NODAL CALCULATION
-
-    ! call assemble_int_forces()
-  ! (1) Knowing the stress, pressure, hourglass forces and shock viscosity at t” in each zone or
-  ! element, the forces at the nodes are calculated. The accelerations of the nodes are
-  ! calculated by dividing the nodal forces by the nodal masses.   
-    !print *, "calc elem forces "
-    call cal_elem_forces()
-
-    if (debug_mode .eqv. .true.) then    
-    do e=1, elem_count
-      do gp = 1, elem%gausspc(e)
-      !print *, "elem%dHxy_detJ(e,gp,1", elem%dHxy_detJ(e,gp,1,:)
-      !print *, "elem%dHxy_detJ(e,gp,2", elem%dHxy_detJ(e,gp,2,:)
-        do n=1, nodxelem
-          print *, "Node ", n, "Force ", elem%f_int(e,n,:) 
-        end do! nod x elem
-      end do !gp
-    end do!elem      
-    end if  
-    !print *, "assemble int forces "
-    call assemble_forces()
-    
-    !print *, "calc accel "
-    do n=1,node_count
-      do d=1,dim
-        nod%a(n,d) =  (fext_glob(n,d)-rint_glob(n,d))/mdiag(n) 
-      end do 
-    end do
-  if (debug_mode .eqv. .true.) then
-    do n=1,node_count
-      print *, "glob res forces ", (fext_glob(n,:)-rint_glob(n,:))
-    end do 
-  end if
-  call impose_bca
   
-  !!!!! THIS IS NOT SOLVED AS A COMPLETED STEP (REDUCED VERLET=
-  ! (2) The acceleration is integrated to give the velocity at tn+l/2.
-  ! !Update vel with CURRENT ACCELERATION
-  ! THIS WOULD BE AT ONE STEP
-  ! nod%v(n,:) = nod%v(n,:) + dt * 0.5 * (nod%a(n,:) + prev_acc(:)) 
-  ! print *,"node vel ", nod%v(n,:)  
   nod%v = nod%v + dt * nod%a   
   call impose_bcv !!!REINFORCE VELOCITY BC
 
-  if (debug_mode .eqv. .true.) then
-  do n=1,node_count
-    print *, "nod ", n, "a ", nod%a(n,:)  
-  end do  
-  end if 
+
   !!(3) The velocity is integrated to give the displacement at tn+1.
   nod%u = nod%u +  nod%v * dt
   nod%x = nod%x + nod%u
@@ -240,6 +183,24 @@ subroutine SolveLeapfrog (tf, dt)
   ! (5) The artificial shock viscosity and hourglass viscosity are calculated from un+1/2. ATTENTION
   call calc_hourglass_forces
 
+    !print *, "det EXT(e,gp)", elem%detJ(:,:)
+    do n=1,elem_count
+      if (elem%gausspc(n) .eq. 8) then !!!! ELSE IS CONSTANT
+        call calculate_element_shapeMat() !AND MASS
+      end if
+    end do
+
+    call cal_elem_forces()
+    call assemble_forces()
+    
+    !print *, "calc accel "
+    do n=1,node_count
+      do d=1,dim
+        nod%a(n,d) =  (fext_glob(n,d)-rint_glob(n,d))/mdiag(n) 
+      end do 
+    end do
+    call impose_bca
+    
   time = time + dt
   end do !time
 
