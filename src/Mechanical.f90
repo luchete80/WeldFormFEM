caaa28bf4a82f5636b620d66de7da31b7fdf6b53
luchete80 <luciano.buglioni@gmail.com>
2023-06-30 17:29:13 -0300

    fixed eq stress

--- src/Mechanical.f90
+++ src/Mechanical.f90
@@ -409,12 +409,12 @@ subroutine CalcEquivalentStress
   
   do e=1,elem_count
     do gp=1,elem%gausspc(e)
-    J2	= 0.5*(elem%shear_stress(e,gp,0,0)*elem%shear_stress(e,gp,0,0) + &
-          2.0*elem%shear_stress(e,gp,0,1)*elem%shear_stress(e,gp,1,0) + &
-          2.0*elem%shear_stress(e,gp,0,2)*elem%shear_stress(e,gp,2,0) + &
-              elem%shear_stress(e,gp,1,1)*elem%shear_stress(e,gp,1,1) + &
+    J2	= 0.5*(elem%shear_stress(e,gp,1,1)*elem%shear_stress(e,gp,1,1) + &
           2.0*elem%shear_stress(e,gp,1,2)*elem%shear_stress(e,gp,2,1) + &
-              elem%shear_stress(e,gp,2,2)*elem%shear_stress(e,gp,2,2));
+          2.0*elem%shear_stress(e,gp,1,3)*elem%shear_stress(e,gp,3,1) + &
+              elem%shear_stress(e,gp,2,2)*elem%shear_stress(e,gp,2,2) + &
+          2.0*elem%shear_stress(e,gp,2,3)*elem%shear_stress(e,gp,3,2) + &
+              elem%shear_stress(e,gp,3,3)*elem%shear_stress(e,gp,3,3));
     !print *, "J2 ", J2
     elem%sigma_eq(e,gp) = sqrt(3.0*J2)
     end do
