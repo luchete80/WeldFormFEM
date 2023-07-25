650466d66a12c8cc1613cca05fe97b703e86c032
luchete80 <luciano.buglioni@gmail.com>
2023-07-25 17:18:59 -0300

    added domain to Verlet Solve

--- src/Mechanical.f90
+++ src/Mechanical.f90
@@ -342,7 +342,8 @@ subroutine calc_elem_shock_visc (dt)
   gp = 1
   do e = 1, elem_count
     do gp = 1, elem%gausspc(e)
-      elem%p_visc = 0.06 * elem%rho(e,gp) * elem%c_s(e,gp) * elem%vol_inc(e) / dt
+      elem%p_visc(e,gp) = 0.06 * elem%rho(e,gp) * elem%c_s(e,gp) * elem%vol_inc(e) / dt
+      !print *, "elem%p_visc(e,gp) ", elem%p_visc(e,gp)
     end do
   end do
 end subroutine
