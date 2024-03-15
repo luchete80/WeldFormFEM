4d343e0e2d4acd77ce8515f96d7d83b2c7773f3b
INTI-Ubuntu <luciano.buglioni@gmail.com>
2024-03-15 07:56:08 -0300

    added set and get deriv ith gauss pt

--- src/include/Domain_d.h
+++ src/include/Domain_d.h
@@ -202,16 +202,16 @@ __global__ void calcTotMassKernel(Domain_d *dom_d);
 
 //i: dim, j: node
 inline dev_t double & Domain_d::getDerivative(const int &e, const int &gp, const int &i, const int &j){ //I AND J ARE: DIMENSION AND NODE
-      if (i == 0)     return m_dH_detJ_dx[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + i];
-      else if (i==1)  return m_dH_detJ_dy[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + i];
-      else if (i==2)  return m_dH_detJ_dz[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + i];
+      if (i == 0)     return m_dH_detJ_dx[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + i];
+      else if (i==1)  return m_dH_detJ_dy[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + i];
+      else if (i==2)  return m_dH_detJ_dz[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + i];
       else printf ("ERROR: WRONG DERIVATIVE DIMENSION.");
 }
 
 inline dev_t void Domain_d::setDerivative(const int &e, const int &gp, const int &i, const int &j, const double &v){ //I AND J ARE: DIMENSION AND NODE
-      if (i == 0)     m_dH_detJ_dx[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + j] = v;
-      else if (i==1)  m_dH_detJ_dy[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + j] = v;
-      else if (i==2)  m_dH_detJ_dz[e*(m_nodxelem * m_gp_count) + gp * m_gp_count + j] = v;
+      if (i == 0)     m_dH_detJ_dx[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + j] = v;
+      else if (i==1)  m_dH_detJ_dy[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + j] = v;
+      else if (i==2)  m_dH_detJ_dz[e*(m_nodxelem * m_gp_count) + gp * m_nodxelem + j] = v;
       else printf ("ERROR: WRONG DERIVATIVE DIMENSION.");
 }
 
