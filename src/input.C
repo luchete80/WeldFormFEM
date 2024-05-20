03df48d7df26d1b6003f26b828ac4b4e1b608124
luciano nb <luciano.buglioni@gmail.com>
2024-05-20 16:16:25 -0300

    adding ls-dyna reader

---
+++ src/input.C
@@ -0,0 +1,22 @@
+#include "Domain_d.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace LS_Dyna;
+
+namespace MetFEM{
+
+Domain_d::Domain_d(std::string name){
+  cout << "Reading "<<name<<endl;
+  lsdynaReader reader(name.c_str());
+  
+  vector_t *x_H =  new vector_t [reader.m_node.size()];
+  
+  for (int n=0;n<reader.m_node.size();n++){
+    //reader.m_node[n].m_x[0]
+  }
+
+}
+
+};
