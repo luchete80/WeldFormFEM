ac3def6371b360dafb82488a6106413344aee179
INTI-Ubuntu <luciano.buglioni@gmail.com>
2024-09-06 10:56:59 -0300

    Added BUILD CPU

--- src/include/defs.h
+++ src/include/defs.h
@@ -49,10 +49,10 @@
 #define memcpy_t(dest,src,size)   memcpy(dest,src,size)     
 #define OMP_PARA_INTERNAL _Pragma("omp parallel for schedule (static) num_threads(Nproc)")
 
-//#define par_loop(n,upto)   OMP_PARA_INTERNAL\
+#define par_loop(n,upto)   OMP_PARA_INTERNAL\
                             for(int n=0;n<upto;n++)
 
-#define par_loop(n,upto)  for(int n=0;n<upto;n++)
+//#define par_loop(n,upto)  for(int n=0;n<upto;n++)
 #endif
 
 #endif
