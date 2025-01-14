d8235ba5ec7bc68b86c39ae5f93741f47a94bb8b
Luciano Buglioni <33092006+luchete80@users.noreply.github.com>
2025-01-14 15:31:51 -0300

    Update Thermal.C

--- src/Thermal.C
+++ src/Thermal.C
@@ -1,15 +1,21 @@
 #include "Domain_d.h"
 
 Domain_d::ThermalCalcs(){
-Matrix *Kt   = new Matrix(3,3);
-/////Tn+1=Tn+dt*invM(Q-KTn)
+
+//parallel loop here
+for (int e=0)
+  Matrix *Kt   = new Matrix(3,3);
+  /////Tn+1=Tn+dt*invM(Q-KTn)
 //BT x B
 //faster than matmult? 
 
 for (int i=0;i<m_nodxelem;i++)
   for (int j=0;j<m_nodxelem;j++){
-    double e =0.0;
+    double k =0.0;
     for (int d=0;d<m_dim;d++)
-      e+=
+      k +=getDerivative(e,0,i,d)*getDerivative(e,0,j,d);
+    Kt->setVal(i,j,k);
   }
+ }
+ delete
 }
