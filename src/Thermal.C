abbcc0a8e3b82788f6a54cd35b348de3bb988d91
Luciano Buglioni <33092006+luchete80@users.noreply.github.com>
2025-01-14 18:01:11 -0300

    Update Thermal.C adding pl_strain tensor AND  termal work

--- src/Thermal.C
+++ src/Thermal.C
@@ -43,5 +43,45 @@ int offset_t = e * 6 ; //SYM TENSOR
 tensor3 sig= FromFlatSym(m_sigma,          offset_t );
 
 
-
+if (delta_pl_strain > 0.0) {
+		tensor3 depdt = 1./dt*Strain_pl_incr;
+		// Double inner product, Fraser 3-106
+		//cout <<"depdt"<<endl;
+		//cout << depdt<<endl;
+		q_plheat 	= 
+						m_sigma(0,0)*depdt(0,0) + 
+						2.0*Sigma(0,1)*depdt(1,0) + 2.0*Sigma(0,2)*depdt(2,0) + 
+						Sigma(1,1)*depdt(1,1) +
+						2.0*Sigma(1,2)*depdt(2,1) + 
+						Sigma(2,2)*depdt(2,2)
+						;
+		//cout << "plastic heat "<<q_plheat<<endl;
+	}
+  ps_energy[e] += q_plheat * dt;
+}
 }//IHF
+
+
+
+/*
+
+inline void Particle::CalcPlasticWorkHeat(const double &dt){
+	
+	if (delta_pl_strain > 0.0) {
+		Mat3_t depdt = 1./dt*Strain_pl_incr;
+		// Double inner product, Fraser 3-106
+		//cout <<"depdt"<<endl;
+		//cout << depdt<<endl;
+		q_plheat 	= 
+						Sigma(0,0)*depdt(0,0) + 
+						2.0*Sigma(0,1)*depdt(1,0) + 2.0*Sigma(0,2)*depdt(2,0) + 
+						Sigma(1,1)*depdt(1,1) +
+						2.0*Sigma(1,2)*depdt(2,1) + 
+						Sigma(2,2)*depdt(2,2)
+						;
+		//cout << "plastic heat "<<q_plheat<<endl;
+	}
+  ps_energy += q_plheat * dt;
+}
+
+*/
