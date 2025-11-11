1c6214383e1966bc1084ad7d828e2653e0d80993
luciano_nb <luciano.buglioni@gmail.com>
2025-11-11 16:12:40 -0300

    Rename tmp_tensor3.C to Tensor3.C (fix capitalization)

--- src/common/Tensor3.C
+++ src/common/Tensor3.C
@@ -1,21 +1,4 @@
-/*************************************************************************/
-/*  Tensor3.C                                                    */
-/*  WeldformFEM - High-Performance Explicit & Implicit FEM Solvers     */
-/*  (CPU/GPU, C++/CUDA)                                                  */
-/*                                                                       */
-/*  weldform.sph@gmail.com                                                */
-/*  ('https://www.opensourcemech.com',)                                    */
-/*                                                                       */
-/*  Copyright (c) 2023-2025 Luciano Buglioni          */
-/*                                                                       */
-/*  This file is part of the WeldformFEM project.                     */
-/*  Licensed under the GNU General Public License v3.0 or later. */ 
-/*  See the LICENSE file in the project    */
-/*  root for full license information.                                   */
-/*************************************************************************/
-
-
-
+/* 
     Istituto Nazionale di Geofisica e Vulcanologia, Sezione di Catania, Italy
     Électricité de France, Paris, France
     Università di Catania, Catania, Italy
